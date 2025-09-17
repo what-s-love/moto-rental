af9b0cc77526e9e5b17fd7c3f2fe9ed537cd0462
VladislavK <barabanabanan@gmail.com>
2025-09-17 17:57:56 +0400

    initial commit

---
+++ src/main/java/ge/tsepesh/motorental/MotoRentalApplication.java
@@ -0,0 +1,13 @@
+package ge.tsepesh.motorental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoRentalApplication.class, args);
+	}
+
+}
